be37560162750208bd8bd82b4bca547afbbc77af
vebly <>
2024-06-28 20:22:17 +0200

    test

--- startup.lua
+++ startup.lua
@@ -1,5 +1,18 @@
 local awful = require("awful")
-awful.spawn.with_shell("picom")
-awful.spawn.with_shell("./screenlayout.sh")
--- awful.spawn.with_shell("nitrogen --restore &")
+local debug = require("debug")
 
+-- Function to get the current script directory
+local function get_current_script_dir()
+    local info = debug.getinfo(1, "S")
+    local script_path = info.source:sub(2) -- remove the @ character
+    return script_path:match("(.*/)")
+end
+
+-- Get the directory of the current script
+local script_dir = get_current_script_dir()
+
+-- Run picom
+awful.spawn.with_shell("picom")
+
+-- Run the screenlayout script using the relative path
+awful.spawn.with_shell(script_dir .. "screenlayout.sh")
