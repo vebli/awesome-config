1409af12db4d6367111a68cef28460d8a3b8e81f
vebly <>
2024-09-05 20:32:56 +0200

    Removed screenlayout.sh and added nitrogen --restore to startup

--- startup.lua
+++ startup.lua
@@ -1,18 +1,5 @@
 local awful = require("awful")
-local debug = require("debug")
 
--- Function to get the current script directory
-local function get_current_script_dir()
-    local info = debug.getinfo(1, "S")
-    local script_path = info.source:sub(2) -- remove the @ character
-    return script_path:match("(.*/)")
-end
-
--- Get the directory of the current script
-local script_dir = get_current_script_dir()
-
--- Run picom
+awful.spawn.with_shell("nitrogen --restore")
 awful.spawn.with_shell("picom")
 
--- Run the screenlayout script using the relative path
-awful.spawn.with_shell(script_dir .. "screenlayout.sh")
