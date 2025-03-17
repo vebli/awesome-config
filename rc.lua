dcd14eb0914ac2b497a6d3154bd4fdc6d9f0e5d1
vebly <>
2025-03-17 09:35:27 +0100

    rofi powermenu

--- rc.lua
+++ rc.lua
@@ -531,11 +531,11 @@ globalkeys = mytable.join(
         end,
         {description = "rofi launcher", group = "launcher"}),
 
-    -- awful.key({ modkey }, "p", function ()
-    --         os.execute("/home/vebly/.config/rofi/scripts/powermenu_t1")
-    --     end,
-    --     {description = "rofi powermenu", group = "launcher"}),
-    --
+    awful.key({ modkey }, "p", function ()
+            os.execute("/home/vebly/.config/rofi/scripts/powermenu.sh")
+        end,
+        {description = "rofi powermenu", group = "launcher"}),
+
 
     -- Default
     --[[ Menubar
